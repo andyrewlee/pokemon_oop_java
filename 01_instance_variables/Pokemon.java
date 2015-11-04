f6d3ff78a21fc3bdddb5fc4680f0401b57884d76
Andrew Lee <andyrewlee@gmail.com>
2015-11-04 10:02:29 -0800

    First lesson

---
+++ 01_instance_variables/Pokemon.java
@@ -0,0 +1,8 @@
+public class Pokemon
+{
+  // variable within a context of an instance is called instance variables
+  // each object, or instance, has its own copy of instance variables
+  // public means there are no restrictions on how instance variable should be used
+  public String name;
+  public double health;
+}
---
+++ 01_instance_variables/PokemonGame.java
@@ -0,0 +1,30 @@
+public class PokemonGame
+{
+  public static void main(String[] args)
+  {
+    // create instance of Pokemon
+    // an instance of a class is an object
+    // variable pocketMonster references distince Pokemon object
+    // new operator returns memory address of object and places instance
+    // variables inside of the object
+    Pokemon pocketMonster = new Pokemon();
+
+    // set an instance variable by referencing object name followed by a dot
+    // and instance variable's name
+    pocketMonster.name = "Pikachu";
+
+    // get value of instance variable by referencing object name followed by a
+    // dot and instance variable's name
+    System.out.println(pocketMonster.name);
+
+    // we have not set health instance variable yet
+    // java has given it a default value
+    System.out.println(pocketMonster.health);
+
+    // set health instance variable
+    pocketMonster.health = 100;
+
+    // get health instance variable
+    System.out.println(pocketMonster.health);
+  }
+}
